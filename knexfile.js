2c0ec162302688a67339745d7db0ab538499cf10
Michael Linhardt <linhardt.michael@gmail.com>
2022-02-27 03:50:26 +0700

    mod into ES module

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
+import * as config from './src/config/_index'
+
 const env = process.env.NODE_ENV || 'development'
-const envConfigFile = `./src/config/${env}.config.js`
-const envConfig = require(envConfigFile)
-module.exports = { ...envConfig.knex }
+
+export default config[env].knex
