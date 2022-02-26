fbaab5cffd6f4fe7eb48e56711698932299fe05a
Michael Linhardt <linhardt.michael@gmail.com>
2022-02-27 04:33:15 +0700

    add env default setup

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 import * as config from './src/config/_index'
 
-const env = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV
 
 export default config[env].knex
