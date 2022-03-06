ef2e56342d499157e9a4b6bff853de3a048276dd
Michael Linhardt <linhardt.michael@gmail.com>
2022-03-07 06:23:13 +0700

    add config for JWT implementation
    private and public keys here are not intended to be use in production

--- src/application/config/encryption.config.js
+++ src/application/config/encryption.config.js
@@ -1,5 +1,29 @@
+import fs from 'fs'
+import crypto from 'crypto'
+
 export const
 
 	bcrypt = {
 		saltRound: 12,
+	},
+
+	jwtoken = {
+		expireTimes: {
+			authorisation: MS_MINUTE * 5,
+			refresh: MS_WEEK,
+			encryption: MS_SECOND * 10,
+		},
+
+		issuer: 'boilerplate_backend',
+		audience: 'boilerplate_backend',
+		algorithm: 'RS512',
+		headerAlgorithm: 'RSA-OAEP',
+		headerEncryption: 'A256GCM',
+		type: 'JWT',
+
+		privateKeyObject: crypto
+			.createPrivateKey(fs.readFileSync(`${__dirname}/jwtoken/private.jwtoken.key`, 'utf8')),
+		publicKeyObject: crypto
+			.createPublicKey(fs.readFileSync(`${__dirname}/jwtoken/public.jwtoken.key`, 'utf8')),
+
 	}
---
+++ src/application/config/jwtoken/readme.md
@@ -0,0 +1,3 @@
+JW Token keys, create this two files, matching with JSON Web Token standard
+- private.jwtoken.key
+- public.jwtoken.key
