63f9972a602ce0c8e77313ca772c7ba4b14650bf
Michael Linhardt <linhardt.michael@gmail.com>
2022-03-07 06:19:35 +0700

    add and upd constant config

--- src/application/config/_index.js
+++ src/application/config/_index.js
@@ -1,8 +1,7 @@
+import './constants.config'
+
 export * as encryption from './encryption.config'
 export * as development from './development.config'
 export * as production from './production.config'
 export * as test from './test.config'
 
-import { initConstantGlobaly } from './constants.config'
-
-initConstantGlobaly()
--- src/application/config/constants.config.js
+++ src/application/config/constants.config.js
@@ -1,4 +1,10 @@
-export const initConstantGlobaly = () => {
-	global.PUBLIC = true
-	process.stdout.write('Global constants initialized\r\n')
-}
+global.PUBLIC = true
+global.MS_SECOND = 60
+global.MS_MINUTE = 60 * global.MS_SECOND
+global.MS_HOUR = 60 * global.MS_MINUTE
+global.MS_DAY = 24 * global.MS_HOUR
+global.MS_WEEK = 7 * global.MS_DAY
+global.MS_MONTH = 4 * global.MS_WEEK
+global.MS_YEAR = 12 * global.MS_MONTH
+
+process.stdout.write('Global constants initialized\r\n')
