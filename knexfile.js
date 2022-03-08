0a144c9da72074e163eabf4903c6befaa421d0fc
Michael Linhardt <linhardt.michael@gmail.com>
2022-03-09 01:39:33 +0700

    fix config path

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-import * as config from './src/config/_index'
+import * as config from './src/application/config/_index'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const env = process.env.NODE_ENV
