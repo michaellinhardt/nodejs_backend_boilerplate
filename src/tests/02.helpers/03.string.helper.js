5e568a5f45e95866347a92377a949077b4928a36
Michael Linhardt <linhardt.michael@gmail.com>
2022-03-08 22:26:01 +0700

    add string helper and test

---
+++ src/tests/02.helpers/03.string.helper.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai'
+import * as appHelpers from '../../application/helpers/_index'
+import * as h from '../../application/mocha/helpers/_index'
+
+h.tools.printCurrentTestFile(__filename)
+
+const { string } = appHelpers
+
+describe('Testing string helper', () => {
+
+	it('Lower first char', () => {
+		const initialString = 'First Char is Upper'
+		const formatedString = string.lowerFirstChar(initialString)
+		expect(formatedString).to.be.equal('first Char is Upper')
+	})
+
+	it('Upper first char', () => {
+		const initialString = 'first char is Lowwer'
+		const formatedString = string.upperFirstChar(initialString)
+		expect(formatedString).to.be.equal('First char is Lowwer')
+	})
+})
