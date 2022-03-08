a0d7ece8c066da75352aae38741a49717e9814f3
Michael Linhardt <linhardt.michael@gmail.com>
2022-03-09 01:01:27 +0700

    add uuid field in default SQL fields

--- src/application/helpers/knex.helper.js
+++ src/application/helpers/knex.helper.js
@@ -18,6 +18,9 @@ export const
 		table.collate(mysql.collate)
 		table.increments('id').primary()
 
+		table.string('uuid', 36).notNullable()
+		table.unique('uuid')
+
 		table.timestamp('created_at').defaultTo(knex.fn.now())
 		table.timestamp('updated_at')
 			.defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
